030de305540e280254305bc517012f1aa3bcd71f
David Nagl <dnagl.dev@gmail.com>
2023-12-05 12:29:20 +0100

    Added day 5

---
+++ Fertilizer/Program.cs
@@ -0,0 +1,45 @@
+﻿using Fertilizer;
+
+const string inputFile = "input.txt";
+
+Stage1();
+Stage2();
+return;
+
+void Stage1()
+{
+    var lines = File.ReadLines(inputFile).ToList();
+    var maps = ConvertMap.ParseMaps(lines);
+    var seeds = lines[0].Replace("seeds: ", "").Split(" ").Select(long.Parse).ToList();
+
+    var result = seeds.Select(seed => maps.Aggregate(seed, (current, map) => map.ProcessMap(current))).Min();
+    
+    Console.WriteLine($"Stage 1: {result}");
+}
+
+void Stage2()
+{
+    var lines = File.ReadLines(inputFile).ToList();
+    var maps = ConvertMap.ParseMaps(lines);
+    var initialSeeds = lines[0].Replace("seeds: ", "").Split(" ").Select(long.Parse).ToArray();
+
+    var result = long.MaxValue; 
+    
+    var startingSeed = 0l;
+    for (var seedIdx = 0; seedIdx < initialSeeds.Length; seedIdx++)
+    {
+        if (seedIdx % 2 == 0)
+            startingSeed = initialSeeds[seedIdx];
+        else
+        {
+            for (var _ = 0; _ < initialSeeds[seedIdx]; _++)
+            {
+                var seedValue = startingSeed + _;
+                seedValue = maps.Aggregate(seedValue, (current, map) => map.ProcessMap(current));
+                if (result > seedValue)
+                    result = seedValue;
+            }
+        }
+    }
+    Console.WriteLine($"Stage 2: {result}");
+}
--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Collection of code snippets to solve AoC 2023.
 |2| [Cube Conundrum](CubeConundrum/Program.cs) |
 |3| [Gear Ratios](GearRatios/Program.cs)       |
 |4| [Scratchcards](Scratchcards/Program.cs)    |
+|5|[If You Give A Seed A Fertilizer](Fertilizer/Program.cs|
