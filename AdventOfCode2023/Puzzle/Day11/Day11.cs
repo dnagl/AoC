e3adc95a1d4fc7ae01d5f2b5bba8e82d387f4b0a
David Nagl <dnagl.dev@gmail.com>
2023-12-11 07:46:53 +0100

    Add day 11 / part 1

--- AdventOfCode2023/Puzzle/Day11/Day11.cs
+++ AdventOfCode2023/Puzzle/Day11/Day11.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2023.Puzzle.Day11;
+﻿using System.Diagnostics;
+
+namespace AdventOfCode2023.Puzzle.Day11;
 
 [PuzzleInformation(Name ="", Day = 11, Complete = true)]
 public class Day11 : IPuzzle
@@ -13,11 +15,70 @@ public class Day11 : IPuzzle
 
     public string Part1()
     {
-        throw new NotImplementedException();
+        var lines = ExpandMap();
+        
+        var galaxyPositions = new Dictionary<int, Tuple<int, int>>();
+
+        for (var i = 0; i < lines.Count(); i++)
+            for (var j = 0; j < lines[i].Length; j++)
+                if(lines[i][j] == '#')
+                    galaxyPositions.Add(galaxyPositions.Count(), new Tuple<int, int>(i, j));
+
+        var galaxyPairs = new Dictionary<Tuple<int, int>, int>();
+        
+        for (var i = 0; i < galaxyPositions.Count; i++)
+            for (var j = i + 1; j < galaxyPositions.Count(); j++)
+                galaxyPairs.Add(new Tuple<int, int>(i, j), -1);
+
+        foreach (var pair in galaxyPairs)
+        {
+            var galaxy1 = galaxyPositions[pair.Key.Item1];
+            var galaxy2 = galaxyPositions[pair.Key.Item2];
+
+            var xDistance = galaxy1.Item1 < galaxy2.Item1 ? Math.Abs(galaxy2.Item1 - galaxy1.Item1) : Math.Abs(galaxy1.Item1 - galaxy2.Item1);
+            var yDistance = galaxy1.Item2 < galaxy2.Item2 ? Math.Abs(galaxy2.Item2 - galaxy1.Item2) : Math.Abs(galaxy1.Item2 - galaxy2.Item2);
+            galaxyPairs[pair.Key] = xDistance + yDistance;
+        }
+        
+        return galaxyPairs.Values.Sum().ToString();
     }
 
     public string Part2()
     {
-        throw new NotImplementedException();
+        return "";
+    }
+    
+    private List<string> ExpandMap(int factor = 1)
+    {
+        var lines = _lines.ToList();
+
+        var columns = new List<string>();
+
+        for (var i = 0; i < lines[0].Length; i++)
+            columns.Add(string.Join("", lines.Select(x => x[i])));
+
+        var columnsExpanded = new List<string>();
+        for (var i = 0; i < columns.Count; i++)
+        {
+            if (!columns[i].Contains("#"))
+                for(var _ = 0; _ < factor; _++)
+                    columnsExpanded.Add(columns[i]);
+            columnsExpanded.Add(columns[i]);
+        }
+        
+        var rows = new List<string>();
+        for (var i = 0; i < columnsExpanded[0].Length; i++)
+            rows.Add(string.Join("", columnsExpanded.Select(x => x[i])));
+        
+        var rowsExpanded = new List<string>();
+        for (var i = 0; i < rows.Count; i++)
+        {
+            if(!rows[i].Contains("#"))
+                for(var _ = 0; _ < factor; _++)
+                    rowsExpanded.Add(rows[i]);
+            rowsExpanded.Add(rows[i]);
+        }
+
+        return rowsExpanded;
     }
 }
