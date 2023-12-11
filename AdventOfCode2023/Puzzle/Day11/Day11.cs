55e67cbdebf13f9c985f73a94592c86f3049b56b
David Nagl <dnagl.dev@gmail.com>
2023-12-11 14:08:16 +0100

    Update day 11

--- AdventOfCode2023/Puzzle/Day11/Day11.cs
+++ AdventOfCode2023/Puzzle/Day11/Day11.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2023.Puzzle.Day11;
 [PuzzleInformation(Name ="", Day = 11, Complete = true)]
 public class Day11 : IPuzzle
 {
-    private const string Filename = "input_test.txt";
+    private const string Filename = "input.txt";
     private IEnumerable<string> _lines;
     
     public void Setup()
@@ -15,7 +15,8 @@ public class Day11 : IPuzzle
 
     public string Part1()
     {
-        var lines = ExpandMap();
+        var lines = _lines.ToList();
+        var (expandedRows, expandedCols) = GetExpandingRowsAndCols();
         
         var galaxyPositions = new Dictionary<int, Tuple<int, int>>();
 
@@ -35,8 +36,37 @@ public class Day11 : IPuzzle
             var galaxy1 = galaxyPositions[pair.Key.Item1];
             var galaxy2 = galaxyPositions[pair.Key.Item2];
 
-            var xDistance = galaxy1.Item1 < galaxy2.Item1 ? Math.Abs(galaxy2.Item1 - galaxy1.Item1) : Math.Abs(galaxy1.Item1 - galaxy2.Item1);
-            var yDistance = galaxy1.Item2 < galaxy2.Item2 ? Math.Abs(galaxy2.Item2 - galaxy1.Item2) : Math.Abs(galaxy1.Item2 - galaxy2.Item2);
+            int start;
+            int end;
+
+            if (galaxy1.Item1 < galaxy2.Item1)
+            {
+                start = galaxy1.Item1;
+                end = galaxy2.Item1;
+            }
+            else
+            {
+                start = galaxy2.Item1;
+                end = galaxy1.Item1;
+            }
+            
+            var yPath = Enumerable.Range(start, end - start);
+            var yDistance = yPath.Select(x => expandedRows.Contains(x) ? 2 : 1).Sum();
+            
+            if (galaxy1.Item2 < galaxy2.Item2)
+            {
+                start = galaxy1.Item2;
+                end = galaxy2.Item2;
+            }
+            else
+            {
+                start = galaxy2.Item2;
+                end = galaxy1.Item2;
+            }
+            
+            var xPath = Enumerable.Range(start, end - start);
+            var xDistance = xPath.Select(x => expandedCols.Contains(x) ? 2 : 1).Sum();
+            
             galaxyPairs[pair.Key] = xDistance + yDistance;
         }
         
@@ -45,40 +75,81 @@ public class Day11 : IPuzzle
 
     public string Part2()
     {
-        return "";
-    }
-    
-    private List<string> ExpandMap(int factor = 1)
-    {
         var lines = _lines.ToList();
-
-        var columns = new List<string>();
-
-        for (var i = 0; i < lines[0].Length; i++)
-            columns.Add(string.Join("", lines.Select(x => x[i])));
-
-        var columnsExpanded = new List<string>();
-        for (var i = 0; i < columns.Count; i++)
-        {
-            if (!columns[i].Contains("#"))
-                for(var _ = 0; _ < factor; _++)
-                    columnsExpanded.Add(columns[i]);
-            columnsExpanded.Add(columns[i]);
-        }
+        var (expandedRows, expandedCols) = GetExpandingRowsAndCols();
         
-        var rows = new List<string>();
-        for (var i = 0; i < columnsExpanded[0].Length; i++)
-            rows.Add(string.Join("", columnsExpanded.Select(x => x[i])));
+        var galaxyPositions = new Dictionary<int, Tuple<int, int>>();
+
+        for (var i = 0; i < lines.Count(); i++)
+        for (var j = 0; j < lines[i].Length; j++)
+            if(lines[i][j] == '#')
+                galaxyPositions.Add(galaxyPositions.Count(), new Tuple<int, int>(i, j));
+
+        var galaxyPairs = new Dictionary<Tuple<int, int>, int>();
         
-        var rowsExpanded = new List<string>();
-        for (var i = 0; i < rows.Count; i++)
+        for (var i = 0; i < galaxyPositions.Count; i++)
+        for (var j = i + 1; j < galaxyPositions.Count(); j++)
+            galaxyPairs.Add(new Tuple<int, int>(i, j), -1);
+
+        foreach (var pair in galaxyPairs)
         {
-            if(!rows[i].Contains("#"))
-                for(var _ = 0; _ < factor; _++)
-                    rowsExpanded.Add(rows[i]);
-            rowsExpanded.Add(rows[i]);
+            var galaxy1 = galaxyPositions[pair.Key.Item1];
+            var galaxy2 = galaxyPositions[pair.Key.Item2];
+
+            int start;
+            int end;
+
+            if (galaxy1.Item1 < galaxy2.Item1)
+            {
+                start = galaxy1.Item1;
+                end = galaxy2.Item1;
+            }
+            else
+            {
+                start = galaxy2.Item1;
+                end = galaxy1.Item1;
+            }
+            
+            var yPath = Enumerable.Range(start, end - start);
+            var yDistance = yPath.Select(x => expandedRows.Contains(x) ? 1000000 : 1).Sum();
+            
+            if (galaxy1.Item2 < galaxy2.Item2)
+            {
+                start = galaxy1.Item2;
+                end = galaxy2.Item2;
+            }
+            else
+            {
+                start = galaxy2.Item2;
+                end = galaxy1.Item2;
+            }
+            
+            var xPath = Enumerable.Range(start, end - start);
+            var xDistance = xPath.Select(x => expandedCols.Contains(x) ? 1000000 : 1).Sum();
+            
+            galaxyPairs[pair.Key] = xDistance + yDistance;
         }
 
-        return rowsExpanded;
+        var result = 0l;
+        foreach (var pair in galaxyPairs)
+            result += pair.Value;
+        
+        return result.ToString();
+    }
+
+    // (expanded lines | expanded rows)
+    private (List<int>, List<int>) GetExpandingRowsAndCols()
+    {
+        var lines = _lines.ToArray();
+        var result = (new List<int>(), new List<int>());
+
+        for(var i = 0; i < lines.Length; i++)
+            if(!lines[i].Contains('#')) result.Item1.Add(i);
+        
+        for(var i = 0; i < lines[0].Length; i++)
+            if(!string.Join("", lines.Select(x => x[i])).Contains('#'))
+               result.Item2.Add(i);
+        
+        return result;
     }
 }
